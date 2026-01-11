7a9adcbeb233684aa33aa3c74b2538ec66148a5e
unknown <donjohns881@gmail.com>
2026-01-11 10:20:42 +0530

    Added contact section

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Welcome to my portfolio!");
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document
+      .querySelector(link.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+  });
+});
